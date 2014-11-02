60c678c0162345c7835b6812ba17492b11d86a0d
Jeff Hoefs <jeff.hoefs@gmail.com>
2014-11-02 11:06:52 -0800

    sync with StandardFirmata in Firmata v2.4.0-beta.3. formatting via astyle.

--- FirmataStepper.h
+++ FirmataStepper.h
@@ -1,17 +1,17 @@
 /*
   FirmataStepper is a simple non-blocking stepper motor library
   for 2 and 4 wire bipolar and unipolar stepper motor drive circuits
-  as well as EasyDriver (http://schmalzhaus.com/EasyDriver/) and 
+  as well as EasyDriver (http://schmalzhaus.com/EasyDriver/) and
   other step + direction drive circuits.
 
   FirmataStepper (0.1) by Jeff Hoefs
-  
+
   EasyDriver support based on modifications by Chris Coleman
 
   Acceleration / Deceleration algorithms and code based on:
   app note: http://www.atmel.com/dyn/resources/prod_documents/doc8017.pdf
   source code: http://www.atmel.com/dyn/resources/prod_documents/AVR446.zip
-  
+
   stepMotor function based on Stepper.cpp Stepper library for
   Wiring/Arduino created by Tom Igoe, Sebastian Gassner
   David Mellis and Noah Shibley.
@@ -19,11 +19,11 @@
   Relevant notes from Stepper.cpp:
 
   When wiring multiple stepper motors to a microcontroller,
-  you quickly run out of output pins, with each motor requiring 4 connections. 
+  you quickly run out of output pins, with each motor requiring 4 connections.
 
   By making use of the fact that at any time two of the four motor
   coils are the inverse  of the other two, the number of
-  control connections can be reduced from 4 to 2. 
+  control connections can be reduced from 4 to 2.
 
   A slightly modified circuit around a Darlington transistor array or an L293 H-bridge
   connects to only 2 microcontroler pins, inverts the signals received,
@@ -47,7 +47,7 @@
      3  1  0
      4  0  0
 
-  The circuits can be found at 
+  The circuits can be found at
   http://www.arduino.cc/en/Tutorial/Stepper
 */
 
@@ -66,76 +66,80 @@
 #define T1_FREQ_148 ((long)((T1_FREQ*0.676)/100)) // divided by 100 and scaled by 0.676
 
 // library interface description
-class FirmataStepper {
-  public:
-    FirmataStepper(byte interface = FirmataStepper::DRIVER, 
-                    int steps_per_rev = 200, 
-                    byte pin1 = 2, 
-                    byte pin2 = 3, 
-                    byte pin3 = 4, 
-                    byte pin4 = 5);
+class FirmataStepper
+{
+public:
+  FirmataStepper(byte interface = FirmataStepper::DRIVER,
+                 int steps_per_rev = 200,
+                 byte pin1 = 2,
+                 byte pin2 = 3,
+                 byte pin3 = 4,
+                 byte pin4 = 5);
 
-    enum Interface {
-      DRIVER = 1,
-      TWO_WIRE = 2,
-      FOUR_WIRE = 4
-    };
+  enum Interface
+  {
+    DRIVER = 1,
+    TWO_WIRE = 2,
+    FOUR_WIRE = 4
+  };
 
-    enum RunState {
-      STOP = 0,
-      ACCEL = 1,
-      DECEL = 2,
-      RUN = 3
-    };
+  enum RunState
+  {
+    STOP = 0,
+    ACCEL = 1,
+    DECEL = 2,
+    RUN = 3
+  };
 
-    enum Direction {
-      CCW = 0,
-      CW = 1
-    };
+  enum Direction
+  {
+    CCW = 0,
+    CW = 1
+  };
 
-    void setStepsToMove(long steps_to_move, int speed, int accel=0, int decel=0);
+  void setStepsToMove(long steps_to_move, int speed, int accel = 0, int decel = 0);
 
-    // update the stepper position
-		bool update();
+  // update the stepper position
+  bool update();
 
-    byte version(void);
+  byte version(void);
 
-  private:
-    void stepMotor(byte step_num, byte direction);
-    void updateStepPosition();
-    bool running;
-    byte interface;     // Type of interface: DRIVER, TWO_WIRE or FOUR_WIRE
-    byte direction;        // Direction of rotation
-    unsigned long step_delay;    // delay between steps, in microseconds
-    int steps_per_rev;      // number of steps to make one revolution
-    long step_number;        // which step the motor is on
-    long steps_to_move;   // total number of teps to move
-    byte stepDelay;       // delay between steps (default = 1, increase for high current drivers)
+private:
+  void stepMotor(byte step_num, byte direction);
+  void updateStepPosition();
+  bool running;
+  byte interface;     // Type of interface: DRIVER, TWO_WIRE or FOUR_WIRE
+  byte direction;        // Direction of rotation
+  unsigned long step_delay;    // delay between steps, in microseconds
+  int steps_per_rev;      // number of steps to make one revolution
+  long step_number;        // which step the motor is on
+  long steps_to_move;   // total number of teps to move
+  byte stepDelay;       // delay between steps (default = 1, increase for high current drivers)
 
-    byte run_state;
-    int accel_count;
-    long min_delay;
-    long decel_start;
-    int decel_val;
+  byte run_state;
+  int accel_count;
+  long min_delay;
+  long decel_start;
+  int decel_val;
 
-    long lastAccelDelay;
-    unsigned long stepCount;
-    unsigned int rest;
+  long lastAccelDelay;
+  unsigned long stepCount;
+  unsigned int rest;
 
-    float alpha;  // PI * 2 / steps_per_rev
-    long at_x100;  // alpha * T1_FREQ * 100
-    long ax20000;  // alph a* 20000
-    float alpha_x2;  // alpha * 2
-    
-    // motor pin numbers:
-    byte dir_pin;
-    byte step_pin;
-    byte motor_pin_1;
-    byte motor_pin_2;
-    byte motor_pin_3;
-    byte motor_pin_4;
-    
-    unsigned long last_step_time; // time stamp in microseconds of when the last step was taken
+  float alpha;  // PI * 2 / steps_per_rev
+  long at_x100;  // alpha * T1_FREQ * 100
+  long ax20000;  // alph a* 20000
+  float alpha_x2;  // alpha * 2
+
+  // motor pin numbers:
+  byte dir_pin;
+  byte step_pin;
+  byte motor_pin_1;
+  byte motor_pin_2;
+  byte motor_pin_3;
+  byte motor_pin_4;
+
+  unsigned long last_step_time; // time stamp in microseconds of when the last step was taken
 };
 
 #endif
--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 AdvancedFirmata
 ===============
 
-AdvancedFirmata builds on [StandardFirmata](https://github.com/firmata/arduino/tree/master/examples/StandardFirmata) to add additional functionality such 
-as Stepper motor support. The idea behind AdvancedFirmata is to add features 
-that are not yet available in the StandardFirmata (that is included in 
+AdvancedFirmata builds on [StandardFirmata](https://github.com/firmata/arduino/tree/master/examples/StandardFirmata) to add additional functionality such
+as Stepper motor support. The idea behind AdvancedFirmata is to add features
+that are not yet available in the StandardFirmata (that is included in
 Firmata/examples/ in the Arduino IDE) and hopefully add them to the official
-Firmata library at a later time. 
+Firmata library at a later time.
 
-Like StandardFirmata (which is just an Arduino Sketch), AdvancedFirmata relies 
-on the Firmata library included with the Arduino IDE. Therefore Firmata.cpp, 
-Firmata.h or Boards.h should not be changed in order to add a feature to 
+Like StandardFirmata (which is just an Arduino Sketch), AdvancedFirmata relies
+on the Firmata library included with the Arduino IDE. Therefore Firmata.cpp,
+Firmata.h or Boards.h should not be changed in order to add a feature to
 AdvancedFirmata.
 
 In time, some of the featues in AdvancedFirmata may be added to StandardFirmata
@@ -19,11 +19,11 @@ and the official Firmata library.
 To Use
 ---
 
-Clone or download and copy AdvancedFirmata into your Arduino projects/sketch 
-directory (or anywhere on your hard drive). Open in the Arduino IDE, verify and 
+Clone or download and copy AdvancedFirmata into your Arduino projects/sketch
+directory (or anywhere on your hard drive). Open in the Arduino IDE, verify and
 upload to your board.
 
-If you download rather than clone this repository, rename the folder to 
+If you download rather than clone this repository, rename the folder to
 "AdvancedFirmata" after unzipping and before copying into your Arduino projects
 directory.
 
@@ -31,7 +31,7 @@ directory.
 Features (not included in StandardFirmata)
 ---
 
-- Stepper motor support. AdvancedFirmata can be used to drive bipolar and 
+- Stepper motor support. AdvancedFirmata can be used to drive bipolar and
 unipolar stepper motors.
 
 
@@ -46,13 +46,13 @@ Requested Features
 Restrictions
 ---
 
-Due to the large file size, AdvancedFirmata will only run on newer Arduino 
-boards that have > 16KB of flash memory. This is most Arduino and 
+Due to the large file size, AdvancedFirmata will only run on newer Arduino
+boards that have > 16KB of flash memory. This is most Arduino and
 arduino-compatible boards released over the past few years.
 
 
 Contributing
 ---
 
-Please create a branch with the name of the feature you are adding (shiftout, 
+Please create a branch with the name of the feature you are adding (shiftout,
 rotaryencoder, etc) before submitting a pull request.
